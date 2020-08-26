1eff4106b98e0867d1e6048b6623d00a6d456226
victoria andrade <the.kayser03@gmail.com>
2020-08-25 23:45:33 -0500

    segundo commit

---
+++ test/com/glitch/test/prueba.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.glitch.test;
+
+/**
+ *
+ * @author VICTORIA
+ */
+public class prueba {
+    public static void main(String[] args) {
+        System.out.println("Probando git");
+                
+    }
+}
